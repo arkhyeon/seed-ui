9fb0d3c0d194a4d35744b70f0f19b85f2c2b9d9c
KaHyung Kim <rkgud0824.r2ware.com>
2022-08-04 11:04:45 +0900

    [feat]: Button, Tooltip 컴포넌트 export
    1. index.js 파일에 Button, Tooltip export 하는 코드 추가

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,15 @@
 import HeaderCreator from './HeaderCreator';
 import { SetRoute } from './CreateMenu';
 import AsideCreator from './AsideCreator';
+import Button from './Button';
+import Tooltip from './Tooltip';
 
 export { HeaderCreator };
 
 export { SetRoute };
 
 export { AsideCreator };
+
+export { Button };
+
+export { Tooltip };
