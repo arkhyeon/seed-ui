6993f8721e9f4bd6fd1c4bc3686c6268235214dd
qkdwngus <39788908+arkhyeon@users.noreply.github.com>
2022-08-09 21:56:20 +0900

    ##### 제목 - 50자 이내로 요약!
    ### [커밋 타입]: [작업내용]
    
    ##### 본문 - 한 줄에 최대 72 글자까지만 입력하기
    
    # 1. 무엇을 수정했는지
    # 2. 왜 수정했는지
    
    # 꼬릿말은 아래에 작성: ex) #이슈 번호
    -
    #   [커밋 타입]  리스트
    #   feat      : 기능 (새로운 기능)
    #   fix       : 버그 (버그 수정)
    #   refactor  : 리팩토링
    #   style     : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs      : 문서 (문서 추가, 수정, 삭제)
    #   test      : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore     : 기타 변경사항 (빌드 스크립트 수정 등)
    #   post     : 블로그 포스트 추가 (신규 포스트 작성 및 수정)
    # ------------------
    #   [체크리스트]
    #     제목 첫 글자는 대문자로 작성했나요?
    #     제목은 명령문으로 작성했나요?
    #     제목 끝에 마침표(.) 금지
    #     제목과 본문을 한 줄 띄워 분리하기
    #     본문에 여러줄의 메시지를 작성할 땐 "-"로 구분했나요?
    # ------------------

--- src/assets/Work.jsx
+++ src/assets/Work.jsx
@@ -9,12 +9,47 @@ function Work() {
   const [state, setState] = useState({});
 
   const setDataListData = value => {
-    setState({ ...state, value });
+    setState({ id: value });
   };
-
+  const data1 = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+    27, 28, 29, 30,
+  ];
+  const data2 = [
+    'Andi',
+    'Stern',
+    'Andrea',
+    'Ezra',
+    'Romy',
+    'Shaw',
+    'Derek',
+    'Yvonne',
+    'Nils',
+    'Janeen',
+    'Hendrik',
+    'Lucina',
+    'Levi',
+    'Nial',
+    'Bunni',
+    'Sally',
+    'Broddy',
+    'Francklyn',
+    'Hayley',
+    'Erasmus',
+    'Leo',
+    'Lezley',
+    'Merrily',
+    'Leyla',
+    'Brose',
+    'Pearline',
+    'Sherline',
+    'Letitia',
+    'Marianne',
+    'Dael',
+  ];
   return (
     <AsideCreator menuList={DepthList1} title="설정">
-      <Datalist id="ice" setData={setDataListData} />
+      <Datalist id="ice" labelList={data2} setData={setDataListData} />
       <br />
       Hello Work! <br /> <br /> path : {pathname}
       <button
--- src/components/components/Datalist.jsx
+++ src/components/components/Datalist.jsx
@@ -1,34 +1,105 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from '@emotion/styled';
 import { MdKeyboardArrowDown } from 'react-icons/all';
 import TextInput from './TextInput';
 
-function Datalist({ id, setData }) {
+function Datalist({ id, valueList = [], labelList, setData }) {
+  const ref = useRef();
+  const dataListWrapRef = useRef();
+  const dataList = labelList.map((label, i) => {
+    return { value: valueList[i] || label, label };
+  });
+  const [dataListState, setDataListState] = useState(dataList);
+
+  useEffect(() => {
+    document.addEventListener('mousedown', () => {
+      dataListWrapRef.current.style.display = 'none';
+    });
+
+    return () => {
+      document.removeEventListener('mousedown', () => {
+        dataListWrapRef.current.style.display = 'none';
+      });
+    };
+  });
+
+  const searchData = value => {
+    setData(value);
+    const searchList = dataList.filter(data => {
+      return data.label.includes(value);
+    });
+
+    dataListWrapRef.current.style.display = 'block';
+
+    if (searchList[0] === undefined) {
+      dataListWrapRef.current.style.display = 'none';
+    }
+
+    if (searchList[0].label === value && searchList.length === 1) {
+      dataListWrapRef.current.style.display = 'none';
+    }
+
+    console.log(searchList);
+    console.log(searchList.length);
+    console.log(searchList[0]);
+    setDataListState(searchList);
+  };
+
+  const setTextData = value => {
+    ref.current.value = value;
+    searchData(value);
+  };
+
   return (
     <>
       <label htmlFor={id}>{id} : </label>
-      <input id={id} type="text" onChange={e => setData(id, e.target.value)} />
       <TextInput title="hi" value="b" />
       <MdKeyboardArrowDown />
-
       <DataListWrap>
-        <DataListItem value="Chocolate">Chocolate</DataListItem>
-        <DataListItem value="Cookie">Cookie</DataListItem>
-        <DataListItem value="Mint">Mint</DataListItem>
-        <DataListItem value="Vanilla">Vanilla</DataListItem>
-        <DataListItem value="Cake">Cake</DataListItem>
+        <input
+          id={id}
+          ref={ref}
+          type="text"
+          onChange={e => searchData(e.target.value)}
+          autoComplete="off"
+          onFocus={() => {
+            dataListWrapRef.current.style.display = 'block';
+          }}
+        />
+        <DataListItemWrap ref={dataListWrapRef}>
+          {dataListState.map(data => {
+            return (
+              <DataListItem
+                key={data.value}
+                value={data.value}
+                onClick={() => setTextData(data.label)}
+              >
+                {data.label}
+              </DataListItem>
+            );
+          })}
+        </DataListItemWrap>
       </DataListWrap>
     </>
   );
 }
 
-const DataListWrap = styled.ul`
+const DataListWrap = styled.div`
+  width: 100%;
+  background-color: olivedrab;
+`;
+
+const DataListItemWrap = styled.ul`
   width: 100%;
   background-color: darkolivegreen;
 `;
 
 const DataListItem = styled.li`
   width: 100%;
+
+  &:hover {
+    background-color: hotpink;
+  }
 `;
 
 export default Datalist;
