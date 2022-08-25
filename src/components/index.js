09fc288874afc0ef5657df69c3a2a773efcd51d1
KaHyung Kim <rkgud0824.r2ware.com>
2022-08-25 10:06:35 +0900

    [chore]: index.js에 생성한 컴포넌트 모두 export 코드 작성
    1. index.js에 생성한 컴포넌트 모두 export 코드 작성
    
    2. 라이브러리를 통해 컴포넌트 적용 및 테스트를 위함
    
    -

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,16 @@ import { SetRoute } from './CreateMenu';
 import AsideCreator from './AsideCreator';
 import Button from './Button';
 import Tooltip from './Tooltip';
+import Modal from './Modal';
+import alarm from './alarm';
+import Slider from './Slider';
+import DatePicker from './DatePicker';
+import DateBetweenPicker from './DateBetweenPicker';
+import DateTimePicker from './DateTimePicker';
+import DateTimeBetweenPicker from './DateTimeBetweenPicker';
+import TimePicker from './TimePicker';
+import Radio from './Radio';
+import Switch from './Switch';
 
 export { HeaderCreator };
 
@@ -13,3 +23,23 @@ export { AsideCreator };
 export { Button };
 
 export { Tooltip };
+
+export { Modal };
+
+export { alarm };
+
+export { Slider };
+
+export { DatePicker };
+
+export { DateBetweenPicker };
+
+export { DateTimePicker };
+
+export { DateTimeBetweenPicker };
+
+export { TimePicker };
+
+export { Radio };
+
+export { Switch };
