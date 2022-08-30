5d157606f3190b1761d7a0c0c0172f41f201b5d5
KaHyung Kim <rkgud0824.r2ware.com>
2022-08-30 09:05:14 +0900

    [feat]: Toggle 컴포넌트 주석 추가
    1. Toggle.jsx 파일에 인자에 대한 주석 추가
    
    2. 코드에 대한 가독성 높이기 위해 추가
    
    -

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -2,7 +2,52 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
 
-function Toggle({ list = ['아이템 없음'], padding = '12px', value, setValue, style = {} }) {
+/**
+ * @param {String []} param.list
+ * 토글에 들어갈 텍스트가 담긴 배열
+ * default 값은 ['아이템 없음']
+ * @param {String} param.padding
+ * 버튼에서 텍스트가 가지는 여백 (버튼의 크기 관련)
+ * default 값은 '12px'
+ * @param {Number} param.value
+ * 선택된 버튼의 값
+ * useState로 관리되는 상태값 이여야 함
+ * default 값은 1
+ * @param {Function} param.setValue
+ * 선택된 버튼의 값을 바꾸는 함수
+ * useState로 생성된 상태 관리 함수여야 함
+ * default 값은 null
+ * @param {String} param.style.bgColor
+ * 토글 버튼의 기본 색
+ * default 값은 '#eee'
+ * @param {String} param.style.fontColor
+ * 토글 버튼의 텍스트 기본 색
+ * default 값은 'black'
+ * @param {String} param.style.fontSize
+ * 토글 버튼의 텍스트 크기
+ * default 값은 '1rem'
+ * @param {String} param.style.hoverColor
+ * 토글 버튼에 커서 올렸을 때, 버튼의 색
+ * default 값은 'white'
+ * @param {String} param.style.hoverFontColor
+ * 토글 버튼에 커서 올렸을 때, 텍스트의 색
+ * default 값은 'white'
+ * @param {String} param.style.clickedColor
+ * 선택된 버튼의 색
+ * default 값은 '#3498db'
+ * @param {String} param.style.clickedFontColor
+ * 선택된 버튼의 텍스트 색
+ * default 값은 'white'
+ * @returns {JSX.Element} Toggle Component
+ */
+
+function Toggle({
+  list = ['아이템 없음'],
+  padding = '12px',
+  value = 1,
+  setValue = null,
+  style = {},
+}) {
   const {
     bgColor = '#eee',
     fontColor = 'black',
