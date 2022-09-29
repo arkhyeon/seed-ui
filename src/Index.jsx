67625da68306dc3fd106ed13183cb279abcd4ded
KaHyung Kim <rkgud0824.r2ware.com>
2022-09-29 13:33:01 +0900

    [feat]: ItemList 컴포넌트 추가
    1. ItemList.jsx, Item.jsx, Head.jsx 파일 추가
    2. 그룹 리스트 등을 사용할 때 레이아웃 통일을 위함
    
    -

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,48 +1,15 @@
 import React, { useState } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
-import { AiOutlineLeft } from 'react-icons/ai';
+import styled from '@emotion/styled';
 import { DepthList1 } from './assets/DepthMenuList';
 import { HeaderCreator } from './components';
-import DateTimePicker from './components/DateTimePicker';
-import DatePicker from './components/DatePicker';
-import DateTimeBetweenPicker from './components/DateTimeBetweenPicker';
-import DateBetweenPicker from './components/DateBetweenPicker';
-import TimePicker from './components/TimePicker';
-import Radio from './components/Radio';
-import Switch from './components/Switch';
-import alarm from './components/alarm';
-import Tooltip from './components/Tooltip';
-import Toggle from './components/Toggle';
-import Modal from './components/Modal';
-import InputGrid from './components/InputGrid';
-import WhiteButton from './components/Button/WhiteButton';
-import BlackButton from './components/Button/BlackButton';
+
 import Logo from './assets/Logo';
+import ItemList from './components/ItemList/ItemList';
 
 function Index() {
-  const [date1, setDate1] = useState(new Date());
-
-  const [startDate1, setStartDate1] = useState(new Date());
-  const [endDate1, setEndDate1] = useState(new Date());
-
-  const [date2, setDate2] = useState(new Date());
-
-  const [startDate2, setStartDate2] = useState(new Date());
-  const [endDate2, setEndDate2] = useState(new Date());
-
-  const offset = new Date().getTimezoneOffset() * 60000;
-
-  const [currentTime, setCurrentTime] = useState('00:00');
-
-  const [radio, setRadio] = useState(0);
-  const [switch1, setSwitch1] = useState(true);
-  const [switch2, setSwitch2] = useState(true);
-
-  const [toggleValue, setToggleValue] = useState(1);
-
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const handleClose = () => {
-    setIsModalOpen(false);
+  const click = id => {
+    console.log(id);
   };
 
   return (
@@ -63,78 +30,29 @@ function Index() {
 
       <Outlet />
 
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          height: '400px',
-          justifyContent: 'space-between',
-        }}
-      >
-        <div>날짜 선택</div>
-        <DatePicker date={date1} setDate={setDate1} />
-
-        <div>시작, 종료 날짜 선택</div>
-        <DateBetweenPicker
-          startDate={startDate1}
-          setStartDate={setStartDate1}
-          endDate={endDate1}
-          setEndDate={setEndDate1}
+      <Container>
+        <ItemList
+          title="그룹"
+          itemList={[
+            { id: 1, value: 'item_1' },
+            { id: 2, value: 'item_2' },
+            { id: 3, value: 'item_3' },
+          ]}
+          click={click}
         />
-
-        <div>날짜와 시간 선택</div>
-        <DateTimePicker date={date2} setDate={setDate2} />
-
-        <div>시작, 종료 날짜 및 시간 선택</div>
-        <DateTimeBetweenPicker
-          startDate={startDate2}
-          setStartDate={setStartDate2}
-          endDate={endDate2}
-          setEndDate={setEndDate2}
-        />
-
-        <div>시간 선택</div>
-        <TimePicker time={currentTime} setTime={setCurrentTime} />
-
-        <Radio
-          value={radio}
-          setValue={setRadio}
-          list={['test1', 'test2', 'test3']}
-          text="답을 선택해주세요."
-          type="border"
-        />
-        <Switch value={switch1} setValue={setSwitch1} color="green" />
-        <Switch value={switch2} setValue={setSwitch2} color="green" size="small" />
-        <Switch value={switch1} setValue={setSwitch1} color="blue" />
-        <Switch value={switch2} setValue={setSwitch2} color="blue" size="small" />
-      </div>
-
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <Tooltip>툴팁보기</Tooltip>
-      </div>
-      <Toggle
-        list={['테이블 설정', '레이아웃 설정', '테']}
-        setValue={setToggleValue}
-        value={toggleValue}
-        style={{ fontSize: '0.9rem' }}
-      />
-      <BlackButton>등록</BlackButton>
-      {isModalOpen && <Modal modalState={isModalOpen} handleClose={handleClose} />}
-      <InputGrid
-        list={[
-          { subject: 'test1', content: <input /> },
-          { subject: 'test2', content: <input /> },
-          { subject: 'test3', content: <input /> },
-          { subject: 'test4', content: <input /> },
-          [
-            { subject: 'test4', content: <input /> },
-            { subject: 'test4', content: <input /> },
-            { subject: 'test4', content: <input /> },
-          ],
-        ]}
-      />
+      </Container>
     </div>
   );
 }
+
+const Container = styled.div`
+  background: orange;
+  width: 1200px;
+  height: 600px;
+  position: absolute;
+  top: 10%;
+  left: 10%;
+  border: 1px solid black;
+`;
 
 export default Index;
