e673093be28e1b9acf687a1748bbd35c6ce03945
KaHyung Kim <rkgud0824.r2ware.com>
2022-10-05 13:18:28 +0900

    [style]: ItemList 햄버거 버튼 호버 스타일 추가
    1. ItemList 컴포넌트의 햄버거 버튼에 호버 스타일 추가
    
    -

--- src/components/ItemList/Head.jsx
+++ src/components/ItemList/Head.jsx
@@ -24,6 +24,25 @@ const Icon = styled.div`
   cursor: pointer;
   position: absolute;
   right: 10px;
+  width: 20px;
+  height: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+
+  svg {
+    width: 15px;
+    height: 15px;
+  }
+
+  :hover {
+    background: #d2d2d2;
+  }
+
+  :active {
+    background: #808080;
+  }
 `;
 
 export default Head;
