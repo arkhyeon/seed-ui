bce32438a88aebf0cb89cf7005ced6729da47842
KaHyung Kim <rkgud0824.r2ware.com>
2022-10-26 16:38:36 +0900

    [refactor]: Toggle 컴포넌트 useCallback 적용
    1. Toggle 컴포넌트 내 함수들에 useCallback 적용
    
    -

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import styled from '@emotion/styled';
 
 function Toggle({ list = ['아이템 없음'], value = 1, setValue = null }) {
@@ -17,11 +17,14 @@ function Toggle({ list = ['아이템 없음'], value = 1, setValue = null }) {
    * @returns {JSX.Element} Toggle Component
    */
 
-  const handleToggle = btnValue => {
-    setValue(btnValue);
-  };
+  const handleToggle = useCallback(
+    btnValue => {
+      setValue(btnValue);
+    },
+    [setValue],
+  );
 
-  const renderBtn = () => {
+  const renderBtn = useCallback(() => {
     return (
       <>
         {list.map((el, idx) => (
@@ -36,7 +39,7 @@ function Toggle({ list = ['아이템 없음'], value = 1, setValue = null }) {
         ))}
       </>
     );
-  };
+  }, [handleToggle, list, value]);
 
   return <Wrapper>{renderBtn()}</Wrapper>;
 }
