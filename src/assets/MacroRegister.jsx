6e7266f2e00efe1a3de4a226e7e8d396f5902df0
qkdwngus <arkhyeon@gmail.com>
2023-02-09 17:05:26 +0900

    [추가] Card 컴포넌트 추가
     - 카드 형식의 UI

--- src/assets/MacroRegister.jsx
+++ src/assets/MacroRegister.jsx
@@ -1,13 +1,250 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import styled from '@emotion/styled';
+import Card from '../components/Card/Card/Card';
+import { BlackButton, RadioButton } from '../components';
 
 function MacroRegister() {
   const { pathname } = useLocation();
+
+  const [workGroupList, setWorkGroupList] = useState([
+    {
+      wid: 0,
+      h: '업무그룹1',
+      seq: 1,
+      date: '2022-12-09 12:32:54',
+      b: [
+        { name: '프로젝트1', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트2', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트3', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트4', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트5', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트6', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트7', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트8', date: '2022-12-09 12:32:54' },
+      ],
+    },
+    {
+      wid: 1,
+      h: '업무그룹2',
+      seq: 2,
+      date: '2022-12-09 12:32:54',
+      b: [
+        { name: '프로젝트1', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트2', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트3', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트4', date: '2022-12-09 12:32:54' },
+      ],
+    },
+    {
+      wid: 2,
+      h: '업무그룹3',
+      seq: 3,
+      date: '2022-12-09 12:32:54',
+      b: [
+        { name: '프로젝트1', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트2', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트3', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트4', date: '2022-12-09 12:32:54' },
+      ],
+    },
+    {
+      wid: 3,
+      h: '업무그룹4',
+      seq: 4,
+      date: '2022-12-09 12:32:54',
+      b: [
+        { name: '프로젝트1', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트2', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트3', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트4', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트5', date: '2022-12-09 12:32:54' },
+      ],
+    },
+    {
+      wid: 4,
+      h: '업무그룹5',
+      seq: 5,
+      date: '2022-12-09 12:32:54',
+      b: [
+        { name: '프로젝트1', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트2', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트3', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트4', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트5', date: '2022-12-09 12:32:54' },
+      ],
+    },
+    {
+      wid: 5,
+      h: '업무그룹6',
+      seq: 5,
+      date: '2022-12-09 12:32:54',
+      b: [
+        { name: '프로젝트1', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트2', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트3', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트4', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트5', date: '2022-12-09 12:32:54' },
+      ],
+    },
+    {
+      wid: 6,
+      h: '업무그룹7',
+      seq: 5,
+      date: '2022-12-09 12:32:54',
+      b: [
+        { name: '프로젝트1', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트2', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트3', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트4', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트5', date: '2022-12-09 12:32:54' },
+      ],
+    },
+    {
+      wid: 7,
+      h: '업무그룹8',
+      seq: 5,
+      date: '2022-12-09 12:32:54',
+      b: [
+        { name: '프로젝트1', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트2', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트3', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트4', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트5', date: '2022-12-09 12:32:54' },
+      ],
+    },
+    {
+      wid: 8,
+      h: '업무그룹9',
+      seq: 5,
+      date: '2022-12-09 12:32:54',
+      b: [
+        { name: '프로젝트1', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트2', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트3', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트4', date: '2022-12-09 12:32:54' },
+        { name: '프로젝트5', date: '2022-12-09 12:32:54' },
+      ],
+    },
+  ]);
+
+  const [allCollapse, setAllCollapse] = useState(false);
+
   return (
-    <div>
-      Hello MacroRegister <br /> <br /> path : {pathname}
-    </div>
+    <MRWrap>
+      <RadioButton valueList={['SSH', 'Telnet', 'RCS']} defaultValue="SSH" />
+      <BlackButton
+        onClick={() =>
+          setAllCollapse(prevState => {
+            return !prevState;
+          })
+        }
+      >
+        {allCollapse ? '전체 펼치기' : '전체 접기'}
+      </BlackButton>
+      {workGroupList.map(workGroup => {
+        const { wid, h, date, b } = workGroup;
+        return (
+          <Card key={wid} width={370} height={280}>
+            <Card.Header>
+              <TitleWrapper>
+                {h}
+                <span>{date}</span>
+              </TitleWrapper>
+            </Card.Header>
+            <Card.Body>
+              <WGCardTextWrap>
+                {b.map(work => {
+                  return (
+                    <WGCardText key={work.name}>
+                      <p>{work.name}</p>
+                      <p>{work.date}</p>
+                    </WGCardText>
+                  );
+                })}
+              </WGCardTextWrap>
+            </Card.Body>
+          </Card>
+        );
+      })}
+      {/* {workGroupList.map(workGroup => { */}
+      {/*  const { wid, h, date, b } = workGroup; */}
+      {/*  return ( */}
+      {/*    <Card key={wid}> */}
+      {/*      <CardHeader> */}
+      {/*        <TitleWrapper> */}
+      {/*          {h} */}
+      {/*          <span>{date}</span> */}
+      {/*        </TitleWrapper> */}
+      {/*      </CardHeader> */}
+      {/*      <CardBody> */}
+      {/*        <WGCardTextWrap> */}
+      {/*          {b.map(work => { */}
+      {/*            return ( */}
+      {/*              <WGCardText key={work.name}> */}
+      {/*                <p>{work.name}</p> */}
+      {/*                <p>{work.date}</p> */}
+      {/*              </WGCardText> */}
+      {/*            ); */}
+      {/*          })} */}
+      {/*        </WGCardTextWrap> */}
+      {/*      </CardBody> */}
+      {/*    </Card> */}
+      {/*  ); */}
+      {/* })} */}
+    </MRWrap>
   );
 }
+
+const MRWrap = styled.div`
+  width: 100%;
+  padding: 15px;
+  box-sizing: border-box;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px;
+`;
+
+const WGCardTextWrap = styled.div`
+  overflow-y: auto;
+  &::-webkit-scrollbar {
+    width: 6px;
+  }
+
+  &::-webkit-scrollbar-track {
+    border-radius: 10px;
+    background-color: #eeeeee;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background-color: #d3d3d3;
+  }
+`;
+
+const TitleWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+
+  span {
+    font-weight: normal;
+    font-size: 14px;
+  }
+`;
+
+const WGCardText = styled.div`
+  height: 40px;
+  padding: 0 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px dashed #e0e0e0;
+
+  & p {
+    font-size: 13px;
+  }
+`;
 
 export default MacroRegister;
---
+++ src/components/Card/Card/Card.jsx
@@ -0,0 +1,131 @@
+import React, { useEffect, useState } from 'react';
+import styled from '@emotion/styled';
+import { IoIosArrowDown } from 'react-icons/all';
+import { css } from '@emotion/react';
+
+const getChild = (children, displayName) =>
+  React.Children.map(children, child => (child.type.displayName === displayName ? child : null));
+
+export default function Card({ width, height, allCollapse, setAllCollapse, children }) {
+  const [collapse, setCollapse] = useState(false);
+
+  useEffect(() => {
+    if (allCollapse === undefined) return;
+    setCollapse(allCollapse);
+  }, [allCollapse]);
+
+  useEffect(() => {
+    if (allCollapse === undefined) return;
+    const cardLength = document.querySelectorAll('.card-wrapper').length;
+    const collapseLength = document.querySelectorAll('.card-collapse').length;
+    if (cardLength === collapseLength) {
+      setAllCollapse(true);
+    }
+
+    if (collapseLength === 0) {
+      setAllCollapse(false);
+    }
+  }, [collapse, setAllCollapse]);
+
+  const header = getChild(children, 'Header');
+  const body = getChild(children, 'Body');
+  const footer = getChild(children, 'Footer');
+
+  return (
+    <CardWrapper className="card-wrapper">
+      <CardTitle>
+        {header}
+        <IoIosArrowDown
+          onClick={() => {
+            setCollapse(prevState => {
+              return !prevState;
+            });
+          }}
+        />
+      </CardTitle>
+      <CardMain className={collapse && 'card-collapse'} width={width} height={height}>
+        {body}
+        {!!footer.length && <CardAddition>{footer}</CardAddition>}
+      </CardMain>
+    </CardWrapper>
+  );
+}
+
+function Header({ children }) {
+  return <>{children}</>;
+}
+
+function Body({ children }) {
+  return <>{children}</>;
+}
+
+function Footer({ children }) {
+  return <>{children}</>;
+}
+
+Header.displayName = 'Header';
+Card.Header = Header;
+
+Body.displayName = 'Body';
+Card.Body = Body;
+
+Footer.displayName = 'Footer';
+Card.Footer = Footer;
+
+const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const CardTitle = styled.div`
+  width: 94.6%;
+  height: 50px;
+  font-size: 15px;
+  position: relative;
+  background: linear-gradient(#606870, #343a40);
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: white;
+  font-weight: bold;
+  padding: 0 15px;
+  box-sizing: border-box;
+
+  svg {
+    font-size: 18px;
+    margin-left: 15px;
+    cursor: pointer;
+  }
+`;
+
+const CardMain = styled.div`
+  ${({ width, height }) => {
+    return css`
+      width: ${width}px;
+      height: ${height}px;
+    `;
+  }};
+  margin-top: -25px;
+  padding: 35px 10px 10px;
+  box-sizing: border-box;
+  border-radius: 5px;
+  border: 1px solid #e0e0e0;
+  box-shadow: rgba(20, 20, 20, 0.12) 0 0.25rem 0.375rem -0.0625rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  transition: 0.3s;
+
+  &.card-collapse {
+    height: 0;
+    padding: 0 10px;
+    border: 0 solid #e0e0e0;
+    margin: 0;
+  }
+`;
+
+const CardAddition = styled.div`
+  width: 100%;
+`;
